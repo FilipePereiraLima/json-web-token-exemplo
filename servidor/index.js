90c1b8b9cd770c258fcf424d4beabc97ae3c5cd4
FilipePereiraLima <filipe.lima4@estudante.ifms.edu.br>
2023-10-20 17:19:21 -0400

    feat: add new features
    fazendo login com usuario cadastrado, criptografando a senha

--- servidor/index.js
+++ servidor/index.js
@@ -1,106 +1,126 @@
-// JWT
-require("dotenv-safe").config();
-const jwt = require('jsonwebtoken');
-var { expressjwt: expressJWT } = require("express-jwt");
-const cors = require('cors');
+  // JWT
+  require("dotenv-safe").config();
+  const jwt = require('jsonwebtoken');
+  var { expressjwt: expressJWT } = require("express-jwt");
+  const cors = require('cors');
 
-var cookieParser = require('cookie-parser')
+  var cookieParser = require('cookie-parser')
 
-const express = require('express');
-const { usuario } = require('./models');
+  const express = require('express');
+  const { usuario } = require('./models');
 
-const app = express();
+  const app = express();
 
-const crypto = require('./crypto');
+  const crypto = require('./crypto');
 
 
-app.set('view engine', 'ejs');
+  app.set('view engine', 'ejs');
 
-app.use(cors());
+  app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-app.use(express.static('public'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true}));
+  app.use(express.static('public'));
 
-app.use(cookieParser());
-app.use(
-  expressJWT({
-    secret: process.env.SECRET,
-    algorithms: ["HS256"],
-    getToken: req => req.cookies.token
-  }).unless({ path: ["/autenticar", "/logar", "/deslogar", "/"] })
-);
+  app.use(cookieParser());
+  app.use(
+    expressJWT({
+      secret: process.env.SECRET,
+      algorithms: ["HS256"],
+      getToken: req => req.cookies.token
+    }).unless({ path: ["/autenticar", "/logar", "/deslogar", "/"] })
+  );
 
-app.get('/autenticar', async function(req, res){
-  res.render('autenticar');
-})
-
-app.get('/', async function(req, res){
-  res.render("home")
-})
-
-app.post('/logar', (req, res) => {
-  if(req.body.usuario == "filipe" && req.body.senha == 12345) {
-
-    const id = 1;
-    const token = jwt.sign({ id }, process.env.SECRET, {
-      expiresIn: 300
-    })
-
-    res.cookie('token', token, {httpOnly:true});
-    return res.json({
-      usuario: req.body.usuario,
-      token: token
-    })
-  }
-  res.status(500).json({mensagem:"login invalido"})
-
-})
-
-app.post('/deslogar', function(req, res) {
-  res.cookie('token', null, {httpOnly:true});
-  return res.json({
-    deslogado:true
+  app.get('/autenticar', async function(req, res){
+    res.render('autenticar');
   })
-})
 
-app.get('/usuarios/listar', async function(req, res) {
-  let usuarios = await usuario.findAll()
-  res.render('listar', {users: usuarios});
+  app.get('/', async function(req, res){
+    res.render("home")
+  })
 
-})
+  app.post('/logar', async function (req, res){
 
-app.get('/usuarios/cadastrar', async function(req, res){
-  res.render('cadastrar');
-})
+    const usuarioname = await usuario.findOne({ where: { usuario: req.body.usuario} });
+    if(usuarioname == null) return res.status(500).json({ message: 'Ocorreu um erro ao buscar o usuário.' });
+    
+    if(req.body.senha === usuarioname.senha){
+      const id = 1;
+      const token = jwt.sign({ id }, process.env.SECRET, {
+        expiresIn: 300
+      })
 
-app.post('/usuarios/cadastrar', async function(req, res){
+      res.cookie('token', token, {httpOnly:true});
+      return res.json({
+        usuario: req.body.usuario,
+          token: token
+      })
 
-    if(req.body.senha == req.body.confisenha){
-      //aqui vai ir a criptografia da senha e mandar no banco de dados
-    }
-  
-    if(req.body.senha == req.body.confisenha){
-      await usuario.create(req.body);
-      res.redirect('/usuarios/cadastrar')
-      
-      let usuariocrypto = req.body;
-
-
-      //vai ter que criar a senha criptografada no banco de dados
-      //vai ter que chamar ela do banco de dados
-      //descriptografar a senha e comparar elas
-      const encrypted_key = crypto.encrypt(usuariocrypto.senha);
-      console.log("senha criptografada: " + encrypted_key);
-  
-      const decrypted_key = crypto.decrypt(encrypted_key);
-      console.log("descryptografada: " + decrypted_key)
-  
     }else{
-    res.status(500).json({mensagem:"As senhas são diferentes"})
-  }
-})
+      return res.render(500).json({ message: 'Ocorreu um erro ao buscar o usuário.' });
+    }
+  })
+    
 
-app.listen(3000, function() {
-  console.log('App de Exemplo escutando na porta 3000!')
-});
+  app.post('/deslogar', function(req, res) {
+    res.cookie('token', null, {httpOnly:true});
+    return res.json({
+      deslogado:true
+    })
+  })
+
+  app.get('/usuarios/listar', async function(req, res) {
+    let usuarios = await usuario.findAll()
+    res.render('listar', {users: usuarios});
+
+  })
+
+  app.get('/usuarios/cadastrar', async function(req, res){
+    res.render('cadastrar');
+  })
+
+  app.post('/usuarios/cadastrar', async function(req, res){
+
+      if(req.body.senha == req.body.confisenha){
+        //aqui vai ir a criptografia da senha e mandar no banco de dados
+
+        const encrypted_key = crypto.encrypt(req.body.senha);
+
+        let usuariocrypto = req.body;
+        usuariocrypto.senha = encrypted_key;
+
+        await usuario.create(usuariocrypto);
+        res.redirect('/usuarios/cadastrar')
+
+        
+
+
+      
+      }else{
+      res.status(500).json({mensagem:"As senhas são diferentes"})
+    }
+  })
+
+  app.listen(3000, function() {
+    console.log('App de Exemplo escutando na porta 3000!')
+  });
+
+  /*if( req.body.usuario === ) {
+
+
+
+
+      const id = 1;
+      const token = jwt.sign({ id }, process.env.SECRET, {
+        expiresIn: 300
+      })
+
+      res.cookie('token', token, {httpOnly:true});
+      return res.json({
+        usuario: req.body.usuario,
+        token: token
+      })
+    }
+    res.status(500).json({mensagem:"login invalido"})
+
+  })*/
